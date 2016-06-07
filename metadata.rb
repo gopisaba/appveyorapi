d78ae59c3104eb92358006b697b9ea9db91a8330
gopisaba <tlk2gopisaba@gmail.com>
2016-06-07 23:13:34 +0100

    Change the logic to accomadate multiple project environment

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tlk2gopisaba@gmail.com'
 license 'all_rights'
 description 'Trigger the deployment in the Appveyor CI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 source_url 'https://github.com/gopisaba/appveyorapi' if respond_to?(:source_url)
 issues_url 'https://github.com/gopisaba/appveyorapi/issues' if
           respond_to?(:issues_url)
