256c8c885822cac5c2251d9eea702916d3219196
gopisaba <tlk2gopisaba@gmail.com>
2016-06-09 14:43:24 +0100

    Add gem in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tlk2gopisaba@gmail.com'
 license 'all_rights'
 description 'Trigger the deployment in the Appveyor CI'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 source_url 'https://github.com/gopisaba/appveyorapi' if respond_to?(:source_url)
 issues_url 'https://github.com/gopisaba/appveyorapi/issues' if
           respond_to?(:issues_url)
@@ -12,3 +12,5 @@ issues_url 'https://github.com/gopisaba/appveyorapi/issues' if
 supports 'windows'
 
 depends 'windows'
+
+gem 'httparty'
